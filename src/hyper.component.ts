871642f7d25835a2933c6ffb125608b5eb7ebeab
Andrey <andrey-ne@mail.ru>
2019-08-28 15:42:24 +0300

    fix dispose

--- src/hyper.component.ts
+++ src/hyper.component.ts
@@ -23,7 +23,7 @@ export abstract class HyperComponent<TState = any, TEvents = any> {
     private _attributesSubject$ = new ReplaySubject<{ name, value }>();
     private _eventsSubject$ = new ReplaySubject<{ args: any; type: keyof TEvents; }>();
     private _elementSubject$: Subject<HTMLElement> = new ReplaySubject<HTMLElement>();
-    private _disconnect$: ReplaySubject<void> = new ReplaySubject<void>();
+    private _disconnect$: Subject<void> = new Subject<void>();
     protected Element$: Observable<HTMLElement> = this._elementSubject$.asObservable().pipe(
         filter(x => !!x)
     );
